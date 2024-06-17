9286a88f99faa412b08b1509899c46070aa4a2d4
21356777 <21356777@studentmail.ucol.ac.nz>
2024-06-18 00:21:28 +1200

    dashboard bars finished

--- Dashboard.php
+++ Dashboard.php
@@ -20,31 +20,33 @@
          <?php include('partials/menubar.php') ?>
 
          <div class="container-summary">
-            <p>Welcome back!</p>
+            <div class="welcome-header">
+               <p><b>Welcome back!</b></p>
+            </div>
 
             <p><b>Weekly Goal</b></p>
             <div class="pbar-border">
-               <div class="pbar-fill" style="width:20%"></div>
+               <div class="pbar-fill" style="background-color:#52FF00;width:20%"></div>
             </div>
             <p><b>Daily Goal</b></p>
             <div class="pbar-border">
-               <div class="pbar-fill" style="width:20%"></div>
+               <div class="pbar-fill" style="background-color:#717FFF;width:80%"></div>
             </div>
             <p><b>Calories</b></p>
             <div class="pbar-border">
-               <div class="pbar-fill" style="width:20%"></div>
+               <div class="pbar-fill" style="background-color:#FF7777;width:88%"></div>
             </div>
             <p><b>Carbs</b></p>
             <div class="pbar-border">
-               <div class="pbar-fill" style="width:20%"></div>
+               <div class="pbar-fill" style="background-color:#FCFF7C;width:45%"></div>
             </div>
             <p><b>Protein</b></p>
             <div class="pbar-border">
-               <div class="pbar-fill" style="width:20%"></div>
+               <div class="pbar-fill" style="background-color:#FFC452;width:76%"></div>
             </div>
             <p><b>Fat</b></p>
             <div class="pbar-border">
-               <div class="pbar-fill" style="width:20%"></div>
+               <div class="pbar-fill" style="background-color:#8AF8FF;width:12%"></div>
             </div>
          </div>
       </body>
